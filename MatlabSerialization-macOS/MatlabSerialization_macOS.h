4b1a59795337f9b386db0069523f68a025b2fd3f
Pho Hale <phohale@gmail.com>
2019-01-30 23:15:36 -0500

    Added preliminary macOS Support

---
+++ MatlabSerialization-macOS/MatlabSerialization_macOS.h
@@ -0,0 +1,19 @@
+//
+//  MatlabSerialization_macOS.h
+//  MatlabSerialization-macOS
+//
+//  Created by Pho Hale on 1/30/19.
+//  Copyright © 2019 Steve. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MatlabSerialization_macOS.
+FOUNDATION_EXPORT double MatlabSerialization_macOSVersionNumber;
+
+//! Project version string for MatlabSerialization_macOS.
+FOUNDATION_EXPORT const unsigned char MatlabSerialization_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MatlabSerialization_macOS/PublicHeader.h>
+
+
